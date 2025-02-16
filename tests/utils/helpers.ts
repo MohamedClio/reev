9a5bd78a01229306105b6f835b504f545353e65d
Mohamed Clio <75623766+MohamedClio@users.noreply.github.com>
2025-02-16 22:26:26 +0100

    Update helpers.ts

--- tests/utils/helpers.ts
+++ tests/utils/helpers.ts
@@ -1,3 +1,4 @@
+/* Function to generate a random Charger serial while assuming that each serial is a 6-digit number */
 export function generateRandomChargerSerial(): string {
   return Math.floor(100000 + Math.random() * 900000).toString();
 }
