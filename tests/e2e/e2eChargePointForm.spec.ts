bb585a4ed97b84ddb789c8f2a4a0eeb717c4d79d
Mohamed Clio <75623766+MohamedClio@users.noreply.github.com>
2025-02-16 22:37:59 +0100

    Update e2eChargePointForm.spec.ts
    adding some comments

--- tests/e2e/e2eChargePointForm.spec.ts
+++ tests/e2e/e2eChargePointForm.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { ChargerInstallationForm } from "../page-objects/chargerInstallationForm";
 
+/*the following test suite deals with multiple positive scenarios and one negative scenario of adding
+a new charger serial, deleting a charging serial, adding multiple serials, and a negative scenario of
+adding an empty serial */
 test.describe("Charge point installation form test cases", () => {
   let chargerInstallationForm: ChargerInstallationForm;
 
@@ -9,17 +12,20 @@ test.describe("Charge point installation form test cases", () => {
     await page.goto("http://localhost:3000/");
   });
 
+  //A test to add a new charger serial
   test("Add serial number", async () => {
     await chargerInstallationForm.deleteAllSerialNumbers();
     await chargerInstallationForm.addSerialNumber();
   });
 
+  //A test to delete a charger serial
   test("Delete serial number", async () => {
     await chargerInstallationForm.deleteAllSerialNumbers();
     await chargerInstallationForm.addSerialNumber();
     await chargerInstallationForm.deleteLastSerialNumber();
   });
 
+  //A test to add 2 charger serials
   test("Add 2 serial numbers and only delete the last one", async () => {
     await chargerInstallationForm.deleteAllSerialNumbers();
     await chargerInstallationForm.addSerialNumber();
